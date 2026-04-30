70d5b751bd6f357832419115eb6a8641c6e2867c
David <khamunetriclark@gmail.com>
2026-04-29 19:17:48 -0600

    address build warnings; add method signatures to asus-wmi stub

--- asus-wmi-stub.c
+++ asus-wmi-stub.c
@@ -10,5 +10,11 @@ EXPORT_SYMBOL_GPL(asus_hid_unregister_listener);
 int asus_hid_event(enum asus_hid_event event) { return 0; }
 EXPORT_SYMBOL_GPL(asus_hid_event);
 
+void set_ally_mcu_hack(enum asus_ally_mcu_hack status) {}
+EXPORT_SYMBOL_GPL(set_ally_mcu_hack);
+
+void set_ally_mcu_powersave(bool enabled) {}
+EXPORT_SYMBOL_GPL(set_ally_mcu_powersave);
+
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("Stub to satisfy hid-asus dependencies on older SteamOS kernels");
